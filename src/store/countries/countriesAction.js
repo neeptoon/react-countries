b906022cc3986aab747268b20143cde37428fde1
Evgenii Shekhanov <evgenii.shekhanov@convergent.digital>
2022-05-25 07:38:47 +0300

    renders countries

--- src/store/countries/countriesAction.js
+++ src/store/countries/countriesAction.js
@@ -1,26 +1,28 @@
+import {ALL_COUNTRIES} from "../../config";
+
 export const SET_COUNTRIES = '@@countries/SET_COUNTRIES';
 export const SET_LOADING = '@@countries/SET_LOADING';
 export const SET_ERROR = '@@countries/SET_ERROR';
 
 
-const setCountries = (countries) => ({
+export const setCountries = (countries) => ({
 	type: SET_COUNTRIES,
 	payload: countries
 })
 
-const setLoading = () => ({
+export const setLoading = () => ({
 	type: SET_LOADING
 })
 
-const setError = (error) => ({
+export const setError = (error) => ({
 	type: SET_ERROR,
 	payload: error
 })
 
-export const lodadCountries = () => (dispatch, _, {client, api}) => {
+export const loadCountries = () => (dispatch, _, {client, api}) => {
 	dispatch(setLoading());
 
 	client.get(api.ALL_COUNTRIES)
-		.then(({data}) => setCountries(data))
-		.catch(error => setError(error))
+		.then(({data}) => dispatch(setCountries(data)))
+		.catch(error => dispatch(setError(error)))
 }
